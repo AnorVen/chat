4871b38cd78c061091c48ea3a5bcce4dbcc3fcd4
Ефремов Георгий Станиславович <Efremov.Georgiy@corp.wildberries.ru>
2018-08-01 13:55:02 +0300

    priter

--- server/sockets.js
+++ server/sockets.js
@@ -1,31 +1,28 @@
-"use strict";
+'use strict'
 
-const MessageModule = require('./models/messages_models');
-module.export = io =>{
-  io.on('connection', function (socket) {
-    socket.emit('connected', 'connect ok');
+const MessageModule = require('./models/messages_models')
+module.export = io => {
+	io.on('connection', function(socket) {
+		socket.emit('connected', 'connect ok')
 
-    socket.join('all');
+		socket.join('all')
 
-    socket.on('msg', content => {
+		socket.on('msg', content => {
+			const obj = {
+				date: new Date(),
+				content: content,
+				username: socket.id,
+			}
 
-
-
-      const obj ={
-        date: new Date(),
-        content: content,
-        username: socket.id
-      };
-
-      MessageModule.create(obj, err => {
-        if(error){
-          console.error('message model', err);
-        }
-        socket.emit('message', obj);
-        socket.to('all').emit('message', obj);
-      });
-      socket.emit('message', obj);
-      socket.to('all').emit('message', obj)
-    })
-  });
-};
+			MessageModule.create(obj, err => {
+				if (error) {
+					console.error('message model', err)
+				}
+				socket.emit('message', obj)
+				socket.to('all').emit('message', obj)
+			})
+			socket.emit('message', obj)
+			socket.to('all').emit('message', obj)
+		})
+	})
+}
